0f20bc1766f7452809531276fd98e2de930bc0d3
wT <2650417+wT-@users.noreply.github.com>
2020-03-17 18:31:04 +0200

    Add small shell install for creating the right symlinks

---
+++ install.sh
@@ -0,0 +1,4 @@
+ln -s ~/dotfiles/fish ~/.config/fish
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish ~/dotfiles/fish/functions/fzf_key_bindings.fish
+# Where does Sublime Text 3 go?
