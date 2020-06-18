848844d33be178fe05d181d3c6250cba29eddaa7
wT <2650417+wT-@users.noreply.github.com>
2020-06-18 22:52:13 +0300

    Add Starship prompt to both Fish and Powershell
    Also add a baseline config for it

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,5 @@
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
-fundle plugin 'matchai/spacefish'
-
-# Leaner than spacefish
-# Fundle install was broken last time I tried
-#fundle plugin 'rafaelrinaldi/pure'
-
 # Bass makes it easy to use utilities written for Bash in fish shell.
 # Example: bass export X=3
 fundle plugin 'edc/bass'
@@ -17,3 +11,6 @@ fundle plugin 'danhper/fish-fastdir'
 fundle plugin 'laughedelic/pisces'
 
 fundle init
+
+# Yet another prompt. Written in Rust. Fast
+starship init fish | source
