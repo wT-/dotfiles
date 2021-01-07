81a2f8b8a59b2195a83df0d5fa16a985dbedd7db
wT <2650417+wT-@users.noreply.github.com>
2021-01-07 05:47:10 +0200

    Update Fish (with mostly disabled plugins...)

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,17 @@
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
+# Prompt. Should use Spaceship instead
+# fundle plugin 'matchai/spacefish'
+
+# Leaner than spacefish
+# Fundle install was broken last time I tried
+# fundle plugin 'rafaelrinaldi/pure'
+
 # Bass makes it easy to use utilities written for Bash in fish shell.
 # Example: bass export X=3
 fundle plugin 'edc/bass'
+# Similar deal
+# fundle plugin 'oh-my-fish/plugin-foreign-env'
 
 # Fish plugin to improve directory navigations. Aliases for .. etc
 fundle plugin 'danhper/fish-fastdir'
@@ -12,8 +21,11 @@ fundle plugin 'laughedelic/pisces'
 
 fundle init
 
-# Hide the fish greeting
-set fish_greeting ""
+# Remove directory truncation (defaults to only 3)
+set -x SPACEFISH_DIR_TRUNC 0
+
+# Completions for Kitty(?)
+kitty + complete setup fish | source
 
 # Yet another prompt. Written in Rust. Fast
 starship init fish | source
