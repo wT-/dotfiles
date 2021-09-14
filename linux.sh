245a02421aa012894b1edd66528246396d8734f9
wT <2650417+wT-@users.noreply.github.com>
2021-09-14 17:35:00 +0300

    Add comment about kitty-themes

--- linux.sh
+++ linux.sh
@@ -1,6 +1,7 @@
 ln -s ~/dotfiles/fish ~/.config/fish
 
 ln -s ~/dotfiles/kitty ~/.config/kitty
+# Make sure the kitty-themes is there too and that the kitty/theme.conf is symlinked properly
 
 ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 
